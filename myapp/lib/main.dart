9d2f3d104b6ef97e966bae3dfc091eda4d24a638
nsnnajam <nsn8287@gmail.com>
2022-02-15 12:27:32 +0500

    first commit

---
+++ myapp/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/homepage.dart';
+
+
+void main(){
+
+runApp(const Myapp());
+
+  
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  const MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home:Homepage()
+
+
+  
+    );
+    
+  }
+}
